280b52bec7fdecd6c6e1a0da4dc60270d0633a86
Spyros Arvanitis <86315072+SPYROS-AR@users.noreply.github.com>
2025-01-30 20:05:51 +0200

    Update it2023003.sh

--- it2023003.sh
+++ it2023003.sh
@@ -9,12 +9,12 @@ OUTPUT_FILE="it2023003.out"
 
 # Function to handle graceful exit
 cleanup() {
-    #check if the pid is not empty and the proccess is running
+    #check if the pid is not empty and the process is running
     if [ -n "$PID" ] && ps -p "$PID" > /dev/null 2>&1; then #silence the output of the ps command
         echo "Terminating process $PID..."
-        kill -9 "$PID"
+        kill SIGTERM "$PID"
     fi
-    echo "Script terminated at $(date)" > /dev/tty
+    echo " Script terminated at $(date)" > /dev/tty
     exit
 }
 
@@ -25,56 +25,54 @@ trap cleanup SIGINT SIGTERM
 # Start the script
 
 #Compile the C program
-if [ ! -e "$OUTPUT_FILE" ];
+if [ ! -e "$OUTPUT_FILE" ]; # check if c file has already been compiled
 then
-    echo "Compiling $C_FILE ..."
     gcc "$C_FILE" -pthread -o "$OUTPUT_FILE"
-
     if [ $? -ne 0 ]; then
-        echo "Compilation failed."
         exit 1
-    else
-        echo "Compilation successful."
-        sleep 5
-        clear
     fi
 fi
 
-
-# Main loop
-while true; do
-    {
-    #find any zombie processes that may exist
-    echo "Checking for zombie processes..."
-    
-    # Find zombie processes
+clean_zombies() {
     zombies=$(ps -eo pid,ppid,stat,comm | awk '$3 ~ /Z/ {print $1, $2}')
+
     if [[ -z "$zombies" ]]; then
         echo "No zombie processes found."
-    else
+        return
+    fi
+
     while read -r pid ppid; do
-        echo "ZOMBIE PROCESS ALARM: parent process to kill: $ppid"
+        echo "ZOMBIE PROCESS DETECTED: Parent process to kill: $ppid"
+
         if ps -p "$ppid" > /dev/null 2>&1; then
-        kill -9 "$ppid"      
+            kill -9 "$ppid"
             if [[ $? -eq 0 ]]; then
                 echo "Successfully killed parent process (PID: $ppid)."
             else
                 echo "Failed to kill parent process (PID: $ppid)."
             fi
-        # else
-        # echo "Parent process (PID: $ppid) is no longer running. Zombie should be cleaned up automatically."
+        else
+            echo "Parent process (PID: $ppid) is already terminated. Zombie should be cleaned up automatically."
         fi
-    done <<< "zombies"
-    fi
+    done <<< "$zombies"
+}
+
+
+# Main loop
+while true; do
+    {
+    clear   
+    #find any zombie processes that may exist
+    echo "Checking for zombie processes..."
+    # Find zombie processes and clean them
+    clean_zombies
+
     # Capture the top 10 processes by CPU usage
     ps -eo pid,pcpu,pmem,rss,vsize,stat,ppid --sort=-%cpu | head -11 > "$LOG_FILE"
 
-
-    echo "Running output file at $(date)"
     ./"$OUTPUT_FILE" &
-    PID=$! #get pid of the ouput file while it is still running
-    wait "$PID" #wait for the proccess to stop running
-        
+    PID=$! #get pid of the output file while it is still running
+    wait "$PID" #wait for the process to stop running
     }
     # Wait for 30 seconds before the next iteration
     sleep 30
